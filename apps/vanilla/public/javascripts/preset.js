66d82f1cc95a2fdb192b2427e201a93971c25b60
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-01-09 10:11:46 +0100

    build: added demo app

---
+++ apps/vanilla/public/javascripts/preset.js
@@ -0,0 +1,30 @@
+function loadPreset(options) {
+    (async () => {
+        await options.initializer(tsParticles);
+
+        await tsParticles.load("tsparticles", { preset: options.name });
+    })();
+
+    const stats = new Stats();
+
+    stats.showPanel(0);
+    stats.dom.style.position = "absolute";
+    stats.dom.style.left = "3px";
+    stats.dom.style.top = "3px";
+    stats.dom.id = "stats-graph";
+
+    const initStats = function () {
+        const update = function () {
+            stats.begin();
+            stats.end();
+
+            requestAnimationFrame(update);
+        };
+
+        requestAnimationFrame(update);
+    };
+
+    document.body.querySelector('#stats').appendChild(stats.dom);
+
+    initStats();
+}
